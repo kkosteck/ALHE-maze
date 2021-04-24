2b1bce31e29e82e91780563beeb9f4b7b43cfded
kkosteck <48053724+kkosteck@users.noreply.github.com>
2021-04-24 02:32:48 +0200

    Working maze generation

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
 import matplotlib.pyplot as plt
 import random
-from timeit import default_timer as timer
 
+import numpy as np
+from timeit import default_timer as timer
 from anytree import AnyNode, RenderTree
 
-N = 10
+N = 5
+M = 10
 
+def getCords(id_1, id_2):
+    if id_2 - id_1 == 1: # vertical edge
+        x = (id_1%M)*2+1
+        y = int(id_1/M) * 2
+    else: # horizontal edge
+        x = (id_1%M) * 2
+        y = int(id_1/M)*2 + 1
+    return x, y
 
-if __name__ == "__main__":
+def fillCorners(area, direction):
+    area[1][1] = 1
+    if direction:
+        if area[0][1] == 1:
+            area[2][1] = 1
+        else:
+            area[0][1] = 1
+    else:
+        if area[1][0] == 1:
+            area[1][2] = 1
+        else:
+            area[1][0] = 1
+    return area
+
+def generateData():
     nodes = []
     for i in range(N):
-        for j in range(N):
-            nodes.append(AnyNode(id=i*N+j+1))
+        for j in range(M):
+            nodes.append(AnyNode(id=i*M+j))
 
     edges = []
     for i in range(N):
-        for j in range(1, N+1):
-            if j != N:
-                edges.append((i*N+j, i*N+j+1))
+        for j in range(M):
+            if j != M-1:
+                edges.append((i*M+j, i*M+j+1))
             if i != N-1:
-                edges.append((i*N+j, (i+1)*N+j))
+                edges.append((i*M+j, (i+1)*M+j))
 
     random.shuffle(edges)
+    return nodes, edges
 
-    maze = []
-    start = timer()
+def generateMaze(nodes, edges):
+    maze = [[0]*(2*M+1) for x in range(2*N+1)]
+
     while(edges):
+        plt.cla()
+        plt.imshow(maze)
+        plt.pause(0.01)
+
         id_1 = edges[-1][0]
         id_2 = edges[-1][1]
         node_1 = next(x for x in nodes if x.id==id_1)
@@ -41,17 +71,28 @@ if __name__ == "__main__":
                 break
 
         if node_1.id == node_2.id:
-            maze.append(edges[-1])
+            x, y = getCords(id_1, id_2)
+
+            area = fillCorners([row[x:x+3] for row in maze[y:y+3]], id_2 - id_1 == 1)
+            for i in range(3):
+                for j in range(3):
+                    maze[y+i][x+j] = area[i][j]
         else:
             temp = list(node_1.children)
             temp.append(node_2)
             node_1.children = temp
         edges.pop()
-    end = timer()
-    print(end - start)
+    
+    for i in range(2*N+1):
+        for j in range(2*M+1):
+            if i == 0 or i == 2*N or j == 0 or j == 2*M:
+                maze[i][j] = 1
 
-    maze.sort()
-    print(maze)
+    return maze
 
-    # plt.imshow(nodes)
-    # plt.show()
+if __name__ == "__main__":
+    nodes, edges = generateData()
+
+    maze = generateMaze(nodes, edges)
+    plt.imshow(maze)
+    plt.show()
